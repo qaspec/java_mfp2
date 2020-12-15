00dc1e94e817ce553be9d4130755fa089519fe69
qaspec <75424351+qaspec@users.noreply.github.com>
2020-12-15 04:22:32 -0500

    A few fuctions have been added

--- sandbox/src/main/java/us/qa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/us/qa/mfp/sandbox/MyFirstProgram.java
@@ -3,6 +3,21 @@ package us.qa.mfp.sandbox;
 public class MyFirstProgram{
 
   public static void main(String[] args){
-    System.out.println("Hello World");
+    hello("Vasya");
+    hello("Dolly");
+    double len= 5;
+    System.out.println("Area of a square with side " + len + " = "+area(len));
+    double x = 5;
+    double y = 7;
+    System.out.println("Area of a rectangle with sides " + x + " and " +y+" = "+area(x,y));
+  }
+    public static void hello(String x){
+      System.out.println("Hello " + x + "!");
+    }
+    public static double area(double l){
+    return l*l;
+    }
+    public static double area (double a, double b){
+    return a*b;
     }
 }
