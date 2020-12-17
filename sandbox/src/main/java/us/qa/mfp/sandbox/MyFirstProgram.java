ae8e8b5e8a6aa52171d28f58c15cf83a99195b22
qaspec <75424351+qaspec@users.noreply.github.com>
2020-12-17 05:03:29 -0500

    Claases Square and Rectangle have been created

--- sandbox/src/main/java/us/qa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/us/qa/mfp/sandbox/MyFirstProgram.java
@@ -5,19 +5,16 @@ public class MyFirstProgram{
   public static void main(String[] args){
     hello("Vasya");
     hello("Dolly");
-    double len= 5;
-    System.out.println("Area of a square with side " + len + " = "+area(len));
-    double x = 5;
-    double y = 7;
-    System.out.println("Area of a rectangle with sides " + x + " and " +y+" = "+area(x,y));
+    Square s = new Square(5);
+
+    System.out.println("Area of a square with side " + s.l + " = "+s.area());
+
+    Rectangle r=new Rectangle(4,6);
+      System.out.println("Area of a rectangle with sides " + r.a + " and " +r.b+" = "+r.area());
   }
     public static void hello(String x){
       System.out.println("Hello " + x + "!");
     }
-    public static double area(double l){
-    return l*l;
-    }
-    public static double area (double a, double b){
-    return a*b;
-    }
+
+
 }
---
+++ sandbox/src/main/java/us/qa/mfp/sandbox/Square.java
@@ -0,0 +1,11 @@
+package us.qa.mfp.sandbox;
+
+public class Square {
+  public double l;
+  public Square(double l){
+    this.l=l;
+  }
+  public double area(){
+    return this.l * this.l;
+  }
+}
